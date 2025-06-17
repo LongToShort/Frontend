7b37a8d795d8e18576127cc6c1af2da9b5a31fb0
yellowmodric <kyr7091@naver.com>
2025-06-17 23:15:22 +0900

    Design: 로그인 페이지 퍼블리싱

--- ltos/src/pages/LoginPage.tsx
+++ ltos/src/pages/LoginPage.tsx
@@ -2,31 +2,49 @@ import { useState } from "react";
 import UserInput from "../component/UserInput";
 import Button from "../component/Button";
 import "../index.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/signup");
+  }
 
   return (
-    <div className="w-sm flex justify-center">
-      <h1 className="text-pink">로그인</h1>
-      <UserInput
-        type="text"
-        placeholder="아이디를 입력해 주세요"
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-      ></UserInput>
-      <br />
-      <UserInput
-        type="password"
-        placeholder="비밀번호를 입력해 주세요"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      ></UserInput>
-      <br />
-      <Button label="로그인"></Button>
-      <br />
-      <Button label="회원가입"></Button>
+    <div className="w-full flex justify-center">
+      <div className="w-sm h-screen border flex flex-col py-[5rem] items-center">
+        <h1 className="text-2xl font-bold mb-1">로그인</h1>
+        <form action="post" className="mt-5 flex flex-col items-center gap-5">
+          <UserInput
+            type="text"
+            placeholder="아이디를 입력해 주세요"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            className="border-2 border-c5-gray rounded-md w-[284px] h-[48px] text-sm pl-4 text-gray-600 font-bold"
+            required
+          ></UserInput>
+          <UserInput
+            type="password"
+            placeholder="비밀번호를 입력해 주세요"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-2 border-c5-gray rounded-md w-[284px] h-[48px] text-sm pl-4 text-gray-600 font-bold"
+            required
+          ></UserInput>
+          <Button
+            label="로그인"
+            className="mt-5 w-[284px] h-[48px] text-center bg-[#548AF7] text-white rounded-md text-md font-bold cursor-pointer"
+          ></Button>
+        </form>
+        <Button
+          onClick={handleClick}
+          label="회원가입"
+          className="mt-4 w-[284px] h-[48px] text-center border-2 border-[#548AF7] text-[#548AF7] rounded-md text-md font-bold cursor-pointer"
+        ></Button>
+      </div>
     </div>
   );
 }
